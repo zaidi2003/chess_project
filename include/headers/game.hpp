7fc3b6713aae676d03137a0a2c290caf4f458890
Ali Abbas <alizaidi22003@gmail.com>
2022-11-30 23:31:41 +0500

    added comments

--- include/headers/game.hpp
+++ include/headers/game.hpp
@@ -1,22 +1,27 @@
 #pragma once
-#include <game.hpp>
 #include <SDL.h>
 #include <iostream>
 #include <stdio.h>
 #include <SDL_image.h>
-
+// imported the SDL Files
 #include <iostream>
 using namespace std;
 
-
+// made class Game
 class Game{
     public:
-        Game(int x,int y,int w,int h,Uint32 flags);
-        void run();
+        Game(int x,int y,int w,int h,Uint32 flags); //initalized all the starting parameters
+        void run(); // public function
     private:
         void init();
         void handleEvents();
+        void display();
+        void render();
+        void startup();
         SDL_Window* window;
         SDL_Renderer* renderer;
         bool gameIsRunning;
 };
+
+
+
--- src/game.cpp
+++ src/game.cpp
@@ -5,16 +5,15 @@
 #include <SDL_image.h>
 
 Game::Game(int x,int y,int w,int h, Uint32 flags){
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO); // initialized SDL2
 
-    window = SDL_CreateWindow("CHESS",x,y,w,h,flags);
+    window = SDL_CreateWindow("CHESS",x,y,w,h,flags); // creates a window using the parameters we gave
     renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
-    run();
+    startup();                 // game startscreen
 }
 
 void Game::run(){
-    gameIsRunning = true;
-    int c= 0;
+    gameIsRunning = true; // gameloop
     while(gameIsRunning){
         handleEvents();
     }
@@ -37,4 +36,31 @@ void Game::handleEvents(){
             }
             
         }
+}
+
+
+void Game::startup(){
+    SDL_Rect rectangle;
+    SDL_SetRenderDrawColor(renderer,0,0,0,SDL_ALPHA_OPAQUE);
+
+    rectangle.x = 0;
+    rectangle.y = 0;
+    rectangle.w = 512;
+    rectangle.h = 512;
+    
+    SDL_Surface* start_screen = IMG_Load("./res/startscreen.png");
+    SDL_Texture* start_screen_texture = SDL_CreateTextureFromSurface(renderer,start_screen);
+    
+    SDL_RenderCopy(renderer,start_screen_texture,NULL,&rectangle);
+
+    display();
+}
+
+
+void Game::render(){
+    cout << " Empty Function ";
+}
+
+void Game::display(){
+    SDL_RenderPresent(renderer); // display my current renderer
 }
